b313f11c7d6099abe61c7c6cc490321a577fa46b
jeissonostos <jeissonandreyo@gmail.com>
2023-06-15 21:25:53 -0500

    Creando la conexion e Administrador

--- Administrador/modelos/administrador.php
+++ Administrador/modelos/administrador.php
@@ -1,5 +1,26 @@
 <?php
+include_once('../../Conexion.php');
+class Administrador extends Conexion{
+	public function __construct(){
+
+    $this->db = parent::__construct(); 
+
+    }
 
 
+    public funcion agregarad($Nombread,$Apellidoad,$Usuarioad,$Passwordad,$Perfilad,$Estadoad)
+    {
+        $statement = $this->db->prepare("INSERT INTO usuarios(nombreusu,apellido,usuusuario,passwordusu,perfil,estado)values(:Nombread,:Apellidoad,:Usuarioad,:Passwordad,:Perfilad,:Estadoad)");
+
+        $statement->bindparam(":Nombread",$Nombread);
+        $statement->bindparam(":Apellidoad",$Apellidoad);
+        $statement->bindparam(":Usuarioad",$Usuarioad);
+        $statement->bindparam(":Passwordad",$Passwordad);
+        $statement->bindparam(":Perfilad",$Perfilad);
+        $statement->bindparam(":Estadoad",$Estadoad);
+    }
+
+   
+}
 
 ?>
---
+++ Conexion.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Conexion{
+
+	protected $db;
+	private $drive= "mysql";
+    private $host= "localhot";
+    private $dbname= "notas2023";
+    private $user="root";
+    private $password= "";
+
+    public function __construct(){
+        try{
+            $db = new PDO("{$this->drive}:host={$this->host};dbname={$this->dbname}",$this->user,$this->password);
+
+            $db->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $db;
+            echo "Conexion realizada";
+
+        }catch(PDOException $e){
+            echo "Se tiene problemaspara conectar".$e->getMessage();
+        }
+    }
+
+}
+
+ ?>
